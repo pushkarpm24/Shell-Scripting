d104baf346aaeca1ce538547f729a251bf6a0b59
pushkarpm24 <61678233+pushkarpm24@users.noreply.github.com>
2020-03-11 11:45:24 +0530

    Update AddTwoNoDice.sh

--- AddTwoNoDice.sh
+++ AddTwoNoDice.sh
@@ -1,7 +1,16 @@
 #!/bin/bash
+
+#problem statement :- Addition of two dice by using random numbers
+#Author :- Pushkar Morey
+#Date :- 9 mar 2020
+
+#Taken two veriables to store the random value of dice 
 diceOne=$(( RANDOM % 6 + 1 ))
 diceTwo=$(( RANDOM % 6 + 1 ))
+
+#Store sum of two dice in veriable sum
 sum=$(( $diceOne + $diceTwo ))
+
 echo "no. 1 is :" $diceOne
 echo "no. 2 is :" $diceTwo
 echo "sum is :" $sum
