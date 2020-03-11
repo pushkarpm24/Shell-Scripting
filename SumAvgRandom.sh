63e8ef9bef31bd467a7f8751a86c9e82ce427c3e
pushkarpm24 <61678233+pushkarpm24@users.noreply.github.com>
2020-03-11 11:59:17 +0530

    Update SumAvgRandom.sh

--- SumAvgRandom.sh
+++ SumAvgRandom.sh
@@ -1,12 +1,20 @@
 #!/bin/bash
 
+#problem statement :- print sum and average of 5 random numbers by using RANDOM
+#Author :- Pushkar Morey
+#Date :- 9 mar 2020
+
+#using 5 variable for getting 5 random values
 num1=$(( RANDOM % 90 + 10 ))
 num2=$(( RANDOM % 90 + 10 ))
 num3=$(( RANDOM % 90 + 10 ))
 num4=$(( RANDOM % 90 + 10 ))
 num5=$(( RANDOM % 90 + 10 ))
 
+#gettin sum of 5 variables
 sum=$(( $num1 + $num2 + $num3 + $num4 + $num5 ))
-echo " sum is :" $sum 
+echo " sum is :" $sum
+
+#getting average of five variable
 avg=$(( $sum / 5 ))
 echo "Avg is :" $avg
