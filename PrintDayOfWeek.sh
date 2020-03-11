dc68d5b2834c92cfaa386333cea55629372c91c3
pushkarpm24 <pushkarmorey555@gmail.com>
2020-03-11 11:16:09 +0530

    Print_Day_Of_Week

--- PrintDayOfWeek.sh
+++ PrintDayOfWeek.sh
@@ -4,6 +4,7 @@ month=$1
 day=$2
 year=$3
 
+#formula for getting day in a week
 y1=$(( year - ( 14 - month ) / 12 ))
 x=$(( y1 - ( y1 / 4 - y1 / 100 + y1 / 400 ) ))
 m1=$(( month + 12 * (( 14 - month ) / 12 ) - 2 ))
