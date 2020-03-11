07c94f2ccf0c8018b537c7797d43faafe0926971
pushkarpm24 <pushkarmorey555@gmail.com>
2020-03-11 11:08:46 +0530

    Addition_of_tow_dice_nos

--- AddTwoNoDice.sh
+++ AddTwoNoDice.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 diceOne=$(( RANDOM % 6 + 1 ))
-diceTwo=$(( RANDOM % 6 + 1))
-sum=$(( $ran + $ran2))
-echo "no. 1 is :" $ran
-echo "no. 2 is :" $ran2
+diceTwo=$(( RANDOM % 6 + 1 ))
+sum=$(( $diceOne + $diceTwo ))
+echo "no. 1 is :" $diceOne
+echo "no. 2 is :" $diceTwo
 echo "sum is :" $sum
 
