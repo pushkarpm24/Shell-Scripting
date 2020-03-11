66d9f339307a606e72172cbccc3ae7e337e93a72
pushkarpm24 <pushkarmorey555@gmail.com>
2020-03-11 11:21:37 +0530

    Sum_And_Avg_Of_Random_No.

--- SumAvgRandom.sh
+++ SumAvgRandom.sh
@@ -6,7 +6,7 @@ num3=$(( RANDOM % 90 + 10 ))
 num4=$(( RANDOM % 90 + 10 ))
 num5=$(( RANDOM % 90 + 10 ))
 
-sum=$(( $ran1 + $ran2 + $ran3 + $ran4 + $ran5 ))
+sum=$(( $num1 + $num2 + $num3 + $num4 + $num5 ))
 echo " sum is :" $sum 
 avg=$(( $sum / 5 ))
 echo "Avg is :" $avg
